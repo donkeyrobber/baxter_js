ec9befd033b5965053389f0ccb9484fec902e59b
RobM <robpmacgregor@gmail.com>
2019-04-28 10:01:49 +0100

    adding basic webpack setup

---
+++ web/client/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    output: {
+       filename: '[name].js',
+       path:  path.resolve(__dirname, 'dist')
+    },
+    entry: path.resolve(__dirname, 'src') + '/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
